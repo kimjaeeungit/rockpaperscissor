3104cf62f949bbbb0387989baba997e5ffcbdf83
Jaeeun Kim <dmjfhe12@gmail.com>
2024-08-13 17:07:16 +0100

    rockpaperscissor1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
+import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Box from './component/Box';
 
+//1. box 2 (title, image, result)
+//2. 가위,바위,보 버튼
+//3. 버튼을 클릭하면 클릭한 값이 박스에 보임
+//4. 컴퓨터는 랜덤하게 아이템 선택이 된다.
+//5. 3, 4의 결과를 가지고 누가 이겼는지 승패를 따진다.
+//6. 승패 결과에 따라 테두리 색이 바뀐다. (이기면-초록, 지면-빨강, 비기면-검은색)
+const choice = {
+  rock: {
+    name: 'Rock',
+    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSY24vXcxwnkJbGJi_nQWV6VNzVronsPHLtUk_dmCc_qHt-sJ9Qziuc82Yp2yOwVPNeOFM&usqp=CAU',
+  },
+  scissors: {
+    name: 'Scissors',
+    img: 'https://www.cleverpatch.com.au/Images/ProductImages/10896.jpg',
+  },
+  paper: {
+    name: 'Paper',
+    img: 'https://i.ebayimg.com/00/s/MTI4MVgxNTAw/z/G5kAAOSwmoRexd9C/$_1.JPG',
+  },
+};
 function App() {
+  const [userSelect, setUserSelect] = useState(null);
+  const [comSelect, setComSelect] = useState(null);
+  const play = (userChoice) => {
+    let comChoice = randomChoice();
+    setUserSelect(choice[userChoice]);
+    setComSelect(comChoice);
+  };
+  // 컴퓨터 선택 랜덤값 뽑기
+  const randomChoice = () => {
+    let itemArray = Object.keys(choice);
+    let randomItem = Math.floor(Math.random() * itemArray.length);
+    let final = itemArray[randomItem];
+    return choice[final];
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="main">
+        <Box title="you" item={userSelect} />
+        <Box title="computer" item={comSelect} />
+      </div>
+      <div className="main">
+        <button onClick={() => play('scissors')}>가위</button>
+        <button onClick={() => play('rock')}>바위</button>
+        <button onClick={() => play('paper')}>보</button>
+      </div>
     </div>
   );
 }
